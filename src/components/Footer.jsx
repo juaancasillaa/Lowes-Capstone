503ecf3c83ac3685094cf7ded166bd1c36a7689f
Vyncent Harris <vyncentharris@AdminisatorsMBP.attlocal.net>
2024-08-27 21:14:20 -0400

    Enhanced file structure and added basic page components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+// src/components/Footer.jsx
+import React from "react";
+import "../css/Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        &copy; THIS IS THE FOOTER 2024 Volunteer Tracking System. All rights
+        reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,14 @@
+// src/pages/SignupPage.jsx
+import React from "react";
+import "../css/SignupPage.css";
+
+function SignupPage() {
+  return (
+    <div className="signup-page">
+      <h1>Sign Up</h1>
+      <p>Create an account to join the Volunteer Tracking System.</p>
+    </div>
+  );
+}
+
+export default SignupPage;
