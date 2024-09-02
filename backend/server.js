463c71c9ef2f0820a8b10d7e67942ba7a8d316ad
madelvilla <villa.lourdes11@gmail.com>
2024-09-02 15:09:27 -0400

    css updates annd database connection

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,9 @@
 const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
-const path = require('path');
 const app = express();
 const port = 5000;
 require('dotenv').config();
-
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, "..", "build")));
-
-// All other requests should send back the React app's index.html
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
-  });
-
-// ----------Database connection setup-----------
 
 // PostgreSQL connection setup
 const pool = new Pool({
@@ -28,16 +17,17 @@ const pool = new Pool({
 app.use(express.json());
 app.use(cors()); // Enable CORS
 
-
 // Login API Route
 app.post('/api/login', async (req, res) => {
   const { email, password } = req.body;
+  console.log('Received login request:', email);
 
   try {
     const result = await pool.query(
       'SELECT * FROM Login WHERE email = $1 AND password = $2',
       [email, password]
     );
+    console.log('Login result:', result.rows);
 
     if (result.rows.length > 0) {
       res.status(200).json({ message: 'Login successful' });
@@ -45,34 +35,11 @@ app.post('/api/login', async (req, res) => {
       res.status(401).json({ error: 'Invalid email or password' });
     }
   } catch (error) {
+    console.error('Database query error:', error.message);
     res.status(500).json({ error: error.message });
   }
 });
 
-// Contact Us API Route
-app.post('/api/contact', async (req, res) => {
-    const { firstName, lastName, phoneNumber, email, comment } = req.body;
-  
-    try {
-      const result = await pool.query(
-        'INSERT INTO contactus (first_name, last_name, phone_number, email, message) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-        [firstName, lastName, phoneNumber, email, comment]
-      );
-  
-      res.status(200).json({ message: 'Form submitted successfully', data: result.rows[0] });
-    } catch (error) {
-      console.error('Error inserting data:', error);
-      res.status(500).json({ error: 'An error occurred. Please try again.' });
-    }
-  });
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-//-----------End Database connection setup-----------
-
-// All other requests should send back the React app's index.html
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
-  });
